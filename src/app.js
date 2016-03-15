9fd50f07aa0aee5be57c56628f7ab8fea707bf91
Fred0510 <frederick5@hotmail.com>
2016-03-15 18:31:43 -0400

    Juego parcialmente terminado

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 
-var HelloWorldLayer = cc.Layer.extend({
-    sprFondo:null,
+    var HelloWorldLayer = cc.Layer.extend({
     Bomb: [ ],
+    Zanahoria: [ ],
+    sprFondo:null,
     sprConejo:null,
     random : function getRandom(min, max){
       return(Math.random() * (max - min + 1)) + min;  
     },
     
-      MoverConejo : function(location,event){
+      MoverConejo : function(location, event){
             cc.log("mover conejo");
             var juego = event.getCurrentTarget();
             var ubicacion = location.getLocation();
@@ -18,22 +19,33 @@ var HelloWorldLayer = cc.Layer.extend({
     
     creaZanahoria : function(){
         var carrot = new cc.Sprite(res.zanahoria_png);
-        carrot.setScale(0.4,0.4);
-        carrot.setPosition(this.random(1,480), this.size.height );    
+       //carrot.setScale(0.4,0.4);
+        carrot.setPosition(this.random());
         this.addChild(carrot, 1);
-        this.carrot.push(carrot);
+        this.Zanahoria.push(carrot);
+        var moveto = cc.moveTo(3,carrot.getPositionX(), -50);
         
     },
     
+//    creaBomba : function(){
+//        var bomb = new cc.Sprite(res.bomba_png);
+//         bomb.setPosition(this.random(1,480), this.size.height );
+//        bomb.setScale(0.4,0.4);
+//        var moveto = cc.moveTo(this.random(1,9), this.Bomb.getPositionX(), this.nave.getPositionY());
+//        this.addChild(bomb,1);
+//        this.Bomb.push(bomb);
+//        
+//    },
     
     creaBomba: function(){
 		
 		var bomb = new cc.Sprite(res.bomba_png);
-		bomb.setScale(0.4,0.4);
-        bomb.setPosition(this.random(1,480), this.size.height );
-        this.addChild(mabel, 1);
-		this.Bomb.push(bomb);		
-		
+		//bomb.setScale(0.4,0.4);
+        bomb.setPosition(this.random(this.size.width/2)-220,(this.size.width/2)+220);
+        this.addChild(bomb, 1);
+		var moveto = cc.moveTo(3,bomb.getPositionX(), -50);
+        bomb.runAction(moveto);
+        this.Bomb.push(bomb);	
 	},
     ctor:function () {
         this._super();
@@ -47,6 +59,8 @@ var HelloWorldLayer = cc.Layer.extend({
         
         //posicionando la imagen de fondo
         this.sprConejo = new cc.Sprite(res.conejo_png);
+        this.schedule(this.creaBomba,3);
+        this.schedule(this.creaZanahoria,3);
         this.sprConejo.setPosition(size.width / 2,size.height * 0.15);
         this.addChild(this.sprConejo, 1);
 
@@ -58,8 +72,6 @@ var HelloWorldLayer = cc.Layer.extend({
 //            
 //        }
 
-        this.schedule(this.creaBomba,3);
-
         var conejo = function(){
          this.sprConejo = new cc.Sprite(res.conejo_png);
         this.sprConejo.setPosition(size.width / 2,size.height * 0.15);
@@ -67,7 +79,6 @@ var HelloWorldLayer = cc.Layer.extend({
             
     }
         
-//        this.schedule(creaZanahoria,3);
         
 //        cc.ActionManager.conejo = this;
 //        cc.ActionManager.right = this.right;
